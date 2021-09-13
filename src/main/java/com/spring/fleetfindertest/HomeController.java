8a8a84df6303bac9d239c66f18d36ec2ad1c55bd
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-09-13 12:18:19 +0300

    Test

--- src/main/java/com/spring/fleetfindertest/HomeController.java
+++ src/main/java/com/spring/fleetfindertest/HomeController.java
@@ -16,7 +16,6 @@ public class HomeController {
 		model.addAttribute("name", name);
         //в ретурне мы должны указать ИМЯ файла шаблона из папки templates который хотим отдать пользователю
 		return "index";
-		System.out.println("This is my branch");
 	}
 
 }
