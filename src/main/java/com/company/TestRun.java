10d39d430da9dfef59922d28762f6fb37130021b
Rad <kalmanjudin2000@gmail.com>
2021-09-16 09:58:00 +0300

    commit

--- src/main/java/com/company/TestRun.java
+++ src/main/java/com/company/TestRun.java
@@ -4,4 +4,5 @@ public class TestRun {
     public static void main(String[] args) {
 
     }
+
 }
--- src/main/java/com/spring/fleetfindertest/HomeController.java
+++ src/main/java/com/spring/fleetfindertest/HomeController.java
@@ -1,5 +1,6 @@
 package com.spring.fleetfindertest;
 
+import net.troja.eve.esi.ApiClientBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
     //GetMapping указывает по какому URL и какой HTTP запрос мы хотим обработать. Может быть например @PostMapping("/submit") или что то типа
 	@GetMapping("/")
-    
     //RequestParam ожидает параметр name в строке браузера(localhost:8080/?name=User) и создает аттрибут name который мы можем отобразить в шаблоне.
 	public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
         //в ретурне мы должны указать ИМЯ файла шаблона из папки templates который хотим отдать пользователю
 		return "index";
 	}
-
 }
