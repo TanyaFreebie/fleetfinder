a6498a78006f9c94788d7e33e88d8ecd0573a98c
Rad <kalmanjudin2000@gmail.com>
2021-09-17 10:24:06 +0300

    commit

--- src/main/java/com/company/TestRun.java
+++ src/main/java/com/company/TestRun.java
@@ -1,8 +1,5 @@
 package com.company;
 
-import com.company.dbHelper.DbConnection;
-import com.company.helpers.User;
-
 public class TestRun {
     public static void main(String[] args) {
         DbConnection.user();
