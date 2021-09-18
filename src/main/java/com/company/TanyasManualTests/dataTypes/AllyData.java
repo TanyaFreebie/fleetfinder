aabfbde7dfc51064c043bf68aec6efe6c47bfa42
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-09-19 02:25:24 +0300

    commit for Kalmans - api requests done except roles - need char for testing Tomorrow will write adding to database logic.

--- src/main/java/com/company/TanyasManualTests/dataTypes/AllyData.java
+++ src/main/java/com/company/TanyasManualTests/dataTypes/AllyData.java
@@ -1,4 +1,40 @@
 package com.company.TanyasManualTests.dataTypes;
 
+import net.troja.eve.esi.ApiException;
+import net.troja.eve.esi.api.AllianceApi;
+import net.troja.eve.esi.api.SsoApi;
+import net.troja.eve.esi.model.AllianceResponse;
+
 public class AllyData {
+private static AllianceApi alliAPI = new AllianceApi();
+
+
+    private static AllianceResponse allyRes(SsoApi api) throws ApiException {
+       return alliAPI.getAlliancesAllianceId(CharData.charAffil(api).get(0).getAllianceId(), " ", null);
+    }
+
+    public static int allyID(SsoApi api) throws ApiException {
+        return CharData.charAffil(api).get(0).getAllianceId();
+    }
+
+    public static String allyName (SsoApi api) throws ApiException{
+        return allyRes(api).getName();
+    }
+
+    public static String allyTicker (SsoApi api) throws ApiException{
+        return allyRes(api).getTicker();
+    }
+
+    public static String allyZKillLink(SsoApi api) throws ApiException {
+        return "https://zkillboard.com/alliance/" + allyID(api) + "/";
+    }
+
+    public static String allyDotlanLink(SsoApi api) throws ApiException {
+        return "https://evemaps.dotlan.net/alliance/" + allyID(api) + "/";
+    }
+
+    public static String allyLogoLink(SsoApi api, int size) throws ApiException {
+        //available sizes 256, 128, 64
+        return "https://images.evetech.net/alliances/" + allyID(api) + "/logo?size=" + size;
+    }
 }
