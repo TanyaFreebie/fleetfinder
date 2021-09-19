6e3d7f60750b19afdee8dcc9c37b6fdf1f601dcf
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-09-19 15:32:57 +0300

    commit for Kalmans - Class Character something wrong with SQL syntax

---
+++ src/main/java/com/company/TanyasManualTests/requestsFromDb/addToDb/Character.java
@@ -0,0 +1,35 @@
+package com.company.TanyasManualTests.requestsFromDb.addToDb;
+
+import com.company.TanyasManualTests.dataTypes.AllyData;
+import com.company.TanyasManualTests.dataTypes.CharData;
+import com.company.TanyasManualTests.dataTypes.CorpData;
+import com.company.dbHelper.dbConnection.DbConnection;
+import com.company.helpers.OutputMessages;
+import net.troja.eve.esi.api.SsoApi;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Character {
+    private static PreparedStatement ps;
+    private static ResultSet rs;
+
+    public static void addNewChar(SsoApi api, String accessToken){
+
+        try {
+            ps = DbConnection.user().prepareStatement("INSERT INTO character (char_id, char_name, total_sp, corp_id, ally_id)" +
+                    " VALUES (?,?,?,?,?)");
+            ps.setInt(1, CharData.charID(api));
+            ps.setString(2,CharData.charName(api));
+            ps.setString(3,CharData.charTotalSkillPoints(api, accessToken));
+            ps.setInt(4, CorpData.corpID(api));
+            ps.setInt(5, AllyData.allyID(api));
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            OutputMessages.error();
+            System.out.println("false");
+        }
+
+    }
+}
