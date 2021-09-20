0fe301862e8b4488954e0c54cb1bad3d8cc74408
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-09-20 20:27:42 +0300

    ready

--- src/main/java/com/company/TanyasManualTests/requestsFromDb/addToDb/AdvertTable.java
+++ src/main/java/com/company/TanyasManualTests/requestsFromDb/addToDb/AdvertTable.java
@@ -5,15 +5,13 @@ import com.company.helpers.OutputMessages;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class AdvertTable {
 
     private static PreparedStatement ps;
     private static ResultSet rs;
 
-    public static void update(int authorID) {
+    public static void author(int authorID) {
         int id = 0;
         try {
             ps = DbConnection.user().prepareStatement("SELECT * FROM advertisements WHERE author_id = " + authorID);
@@ -25,7 +23,6 @@ public class AdvertTable {
         } catch (Exception e) {
             //                           e.printStackTrace();
             OutputMessages.error();
-            System.out.println("true");
         }
 
         if(id!=authorID){addNewAdvert(authorID);}
@@ -44,10 +41,74 @@ public class AdvertTable {
         }
     }
 
-//date format method in case we will need timestamps
-    public static String dateFormat() {
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        return formatter.format(date);
+    public static void advertText(int authorID, String text){
+        try {
+            ps = DbConnection.user().prepareStatement("UPDATE advertisements " +
+                    "SET advert_text = ?  " +
+                    "WHERE author_id = " + authorID);
+            ps.setString(1, text);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            OutputMessages.error();
+        }
+
     }
+
+    public static void specialization(int authorID, String text){
+        try {
+            ps = DbConnection.user().prepareStatement("UPDATE advertisements " +
+                    "SET specialization = ?  " +
+                    "WHERE author_id = " + authorID);
+            ps.setString(1, text);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            OutputMessages.error();
+        }
+    }
+
+    public static void timezone(int authorID, String text){
+        try {
+            ps = DbConnection.user().prepareStatement("UPDATE advertisements " +
+                    "SET timezone = ?  " +
+                    "WHERE author_id = " + authorID);
+            ps.setString(1, text);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            OutputMessages.error();
+        }
+    }
+
+    public static void area(int authorID, String text){
+        try {
+            ps = DbConnection.user().prepareStatement("UPDATE advertisements " +
+                    "SET area = ?  " +
+                    "WHERE author_id = " + authorID);
+            ps.setString(1, text);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            OutputMessages.error();
+        }
+    }
+
+    public static boolean status(int authorID, boolean active){
+
+        try {
+            ps = DbConnection.user().prepareStatement("UPDATE advertisements " +
+                    "SET is_active = ?  " +
+                    "WHERE author_id = " + authorID);
+            ps.setBoolean(1, active);
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            OutputMessages.error();
+        }
+
+        return active;
+    }
+
+
 }
