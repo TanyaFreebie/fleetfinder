8c322084207f8f21a12c5ee011fc6592b0316874
Rad <kalmanjudin2000@gmail.com>
2021-09-20 09:48:08 +0300

    Changed structure of addToDb method from User class.

--- src/main/java/com/spring/fleetfindertest/model/Pilot.java
+++ src/main/java/com/spring/fleetfindertest/model/Pilot.java
@@ -9,23 +9,26 @@ import javax.persistence.*;
 @Entity
 @Table(name = "test")
 public class Pilot {
-
     @Id
     private int charId;
 
     @Column(name = "char_name")
     private String charName;
 
-
     @Column(name = "corp_id")
     private Integer corpId;
 
+    @Column(name = "char_image")
+    private String charImage;
 
+    @Column(name = "corp_name")
+    private String corpName;
 
     @Column(name = "ally_id")
     private Integer allyId;
 
-
+    @Column(name = "ally_name")
+    private String allyName;
     
     //char name
     //skill points
