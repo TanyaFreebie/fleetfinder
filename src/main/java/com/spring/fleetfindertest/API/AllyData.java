1ccb99a40d4ff63a389a5627264dc0503f088867
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-09-23 22:05:40 +0300

    commit

--- src/main/java/com/spring/fleetfindertest/API/AllyData.java
+++ src/main/java/com/spring/fleetfindertest/API/AllyData.java
@@ -1,5 +1,6 @@
 package com.spring.fleetfindertest.API;
 
+import com.spring.fleetfindertest.model.Alliance;
 import net.troja.eve.esi.ApiException;
 import net.troja.eve.esi.api.AllianceApi;
 import net.troja.eve.esi.api.SsoApi;
